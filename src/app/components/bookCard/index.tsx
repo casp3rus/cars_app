e6cb2d70d71ffeaba3b6e7e1f02b204dfe855271
Adrian M. Popan <adrian.popan@gmail.com>
2021-10-03 18:21:54 -0500

    Add Calendar to app
    - add react-calendar as a dependency
    - integrate calendar into the app
    - create book card

---
+++ src/app/components/bookCard/index.tsx
@@ -0,0 +1,102 @@
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { Button } from '../button';
+import { Marginer } from '../marginer';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const CardContainer = styled.div`
+  min-height: 4.3em;
+  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
+  ${tw`
+    flex
+    justify-center
+    items-center
+    rounded-md
+    bg-white
+    pt-1
+    pb-1
+    pr-2
+    pl-2
+    md:pt-2
+    md:pb-2
+    md:pl-9
+    md:pr-9
+  `}
+`;
+
+const ItemContainer = styled.div`
+  ${tw`
+    flex relative
+  `}
+`;
+
+const Icon = styled.span`
+  ${tw`
+    text-red-500
+    fill-current
+    text-xs
+    md:text-base
+    mr-1
+    md:mr-3
+
+  `}
+`;
+
+const LineSeparator = styled.span`
+  width: 2px;
+  height: 45%;
+  ${tw`
+    bg-gray-300
+    mr-2
+    ml-2
+    md:mr-5
+    md:ml-5
+  `}
+`;
+
+const Name = styled.span`
+  ${tw`
+    text-gray-600
+    text-xs
+    md:text-sm
+  `}
+`;
+
+const DateCalendar = styled(Calendar)`
+  position: absolute;
+  max-width: none;
+  top: 3.5em;
+  left: -2em;
+`;
+
+export default function BookCard() {
+  const [startDate, setstartDate] = useState();
+  const [isStartCalendarOpen, setisStartCalendarOpen] = useState(false)
+
+  return (
+    <CardContainer>
+      <ItemContainer>
+        <Icon>
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </Icon>
+        <Name>Pick Up Date</Name>
+        <DateCalendar />
+      </ItemContainer>
+      <LineSeparator />
+      <ItemContainer>
+        <Icon>
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </Icon>
+        <Name>Return Date</Name>
+        <DateCalendar />
+      </ItemContainer>
+      <Marginer direction='horizontal' margin='2em' />
+      <Button text='Book Your Ride' />
+    </CardContainer>
+  );
+}
